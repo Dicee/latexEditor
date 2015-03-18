f726bc5e29f91f8bb9a23a85af4d44298b2fd1f8
Dicee <courtinot.david@orange.fr>
2015-03-18 06:39:53 +0100

    Fixes in the update script

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import re
 import os
 from subprocess import call
 
+"""Load a property file in a dictionary"""
 def load_properties(path):
 	res = {}
 	with open(path,"r") as lines:
@@ -12,6 +13,7 @@ def load_properties(path):
 			res[split[0].strip()] = split[1].strip()
 	return res
 			
+"""Interpret the regular expressions inputted by the user and convert them into Python regular expressions"""
 def read_regexps(args,root):
 	def get_pattern(file):
 		if os.path.isdir(file):
@@ -19,26 +21,28 @@ def read_regexps(args,root):
 			# determine the depth of the directory
 			depth = start + 1
 			for top, dirs, files in os.walk(file):
-				depth = max(top.count(os.sep) - start,depth)
-			
+				depth = max(top.count(os.sep) - start + 1,depth)
+
 			patterns = ["/*"]
 			for i in range(1,depth):
 				patterns.append(patterns[i-1] + "/*")
-			return [ absolute_path(root,file) + x for x in patterns ]
+			return [ file + x for x in patterns ]
 		else:
 			return [ file ]
 	
 	def get_regex(s):
 		return "^" + root + "/" + s.replace(".","\.").replace("*","[^/]+") + "$"
 			
-	return [ re.compile(item) for arg in args for item in get_pattern(arg) ]
+	return [ re.compile(get_regex(item)) for arg in args for item in get_pattern(arg) ]
 
+"""Determine whether a file should be ignored or not"""
 def accept(name,ignore):
 	return not(name.startswith(".")) and name not in ignore and not(name.endswith("jar"))
 	
 def absolute_path(root,subfile):
 	return subfile if root == '.' else root + "/" + subfile
 	
+"""Fetch the list of the absolute path of all the files recursively contained by the given root"""
 def fetch_files(ignore,root):		
 	if not os.path.isdir(root):
 		return frozenset({ root }) if accept(root,ignore) else frozenset()
@@ -46,6 +50,7 @@ def fetch_files(ignore,root):
 	files = { s for s in os.listdir(root) if accept(s,ignore) }
 	return { x for y in files for x in fetch_files(ignore,absolute_path(root,y)) }
 	
+"""Return the list of all the files which path matches one of the given regular expressions"""
 def filter_files(regs,files):
 	matches = []
 	for reg in regs:
@@ -57,6 +62,7 @@ def filter_files(regs,files):
 		print("Found no file matching any of these regular expressions")
 	return matches
 	
+"""Copy all the specified files from from_dir to to_dir. If needed, the appropriate files and folders will be created."""
 def copy_files(files,from_dir,to_dir):
 	# make a backup copy, because I'm cautious : even MY code may contain bugs
 	if not(os.path.exists(to_dir + "/bak")):
@@ -74,13 +80,16 @@ def copy_files(files,from_dir,to_dir):
 		if not os.path.exists(dirs):
 			os.makedirs(dirs)
 		shutil.copyfile(file,path)
+		print("copied :",file[len(from_dir) + 1:])
 	
+"""Print the usage of the command to the user"""
 def print_usage():
 	print("\n--------------\n     USAGE\n--------------")
 	print("\nThis command enables to export (resp. import) files matching a regex to (resp. from) a given Eclipse project from (resp. to) a git repository.")
 	print("\nSyntax : update <operation> regex*")
 	print("\n\nArgument 'operation' can be any of the following : \n  . export\n  . import")
 	
+"""Fail and exit with a message"""
 def fail(msg):
 	print("Failed :",msg)
 	print_usage()
@@ -103,4 +112,6 @@ if __name__ == '__main__':
 	regs     = read_regexps([ f for f in os.listdir(ends[0]) if accept(f,ignore) ],ends[0]) if sys.argv[2] == "-all" else read_regexps(sys.argv[2:],ends[0])
 	files    = fetch_files(ignore,ends[0])
 	selected = filter_files(regs,files)
-	copy_files(selected,ends[0],ends[1])
+
+	if selected:
+		copy_files(selected,ends[0],ends[1])
