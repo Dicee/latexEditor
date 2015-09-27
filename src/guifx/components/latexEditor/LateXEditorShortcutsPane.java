7ec9a9a0d2e65db0b21ad117b26073293b6e3161
Dicee <dcourtinot@gmail.com>
2015-09-27 21:55:27 +0100

    Fixed formatting

--- src/guifx/components/latexEditor/LateXEditorShortcutsPane.java
+++ src/guifx/components/latexEditor/LateXEditorShortcutsPane.java
@@ -19,46 +19,46 @@ import com.dici.javafx.components.IconSelectionBox;
 import com.dici.javafx.components.IconSelectionView;
 
 public class LateXEditorShortcutsPane extends TitledPane {
-	private static final ActionListener NO_OP = new ActionListener() { @Override public void actionPerformed(ActionEvent e) { } };
-	
-	private final IconSelectionView	operators;
-	private final IconSelectionView	greekAlphabet;
+    private static final ActionListener NO_OP = new ActionListener() { @Override public void actionPerformed(ActionEvent e) { } };
+    
+    private final IconSelectionView operators;
+    private final IconSelectionView greekAlphabet;
 
-	public LateXEditorShortcutsPane() {
-		operators     = setOperators();
-		greekAlphabet = setGreekAlphabet();
-		setOnClick(NO_OP);
+    public LateXEditorShortcutsPane() {
+        operators     = setOperators();
+        greekAlphabet = setGreekAlphabet();
+        setOnClick(NO_OP);
 
-		IconSelectionBox box = new IconSelectionBox(operators,greekAlphabet);
-		box.setPadding(new Insets(5,5,5,20));
-		
-		setContent(box);
-		Settings.bindProperty(textProperty(),BOX_TITLE);
-	}
-	
-	private IconSelectionView setOperators() {
-		final String[] ops = { 
-				"\\cdot","+","-","\\frac{}{}","\\sqrt[]{}",
-				"\\forall","\\partial","\\exists","\\nexists","\\varnothing",
-				"\\bigcap","\\bigcup","\\bigint","\\prod","\\sum",
-				"\\nabla","\\in","\\notin","\\ni","",
-				"^{}","_{}","\\leq","\\geq","\\neq",
-				"\\mid\\mid.\\mid\\mid"
-		};
-		return new IconSelectionView(getResourceImage(OPERATORS_ICON),6,5,ops,strings.getObservableProperty(OPERATORS));
-	}
+        IconSelectionBox box = new IconSelectionBox(operators, greekAlphabet);
+        box.setPadding(new Insets(5, 5, 5, 20));
+        
+        setContent(box);
+        Settings.bindProperty(textProperty(), BOX_TITLE);
+    }
+    
+    private IconSelectionView setOperators() {
+        final String[] ops = { 
+                "\\cdot", "+", "-", "\\frac{}{}", "\\sqrt[]{}", 
+                "\\forall", "\\partial", "\\exists", "\\nexists", "\\varnothing", 
+                "\\bigcap", "\\bigcup", "\\bigint", "\\prod", "\\sum", 
+                "\\nabla", "\\in", "\\notin", "\\ni", "", 
+                "^{}", "_{}", "\\leq", "\\geq", "\\neq", 
+                "\\mid\\mid.\\mid\\mid"
+        };
+        return new IconSelectionView(getResourceImage(OPERATORS_ICON), 6, 5, ops, strings.getObservableProperty(OPERATORS));
+    }
 
-	private IconSelectionView setGreekAlphabet() {
-		final String[] ctes = { 
-				"\\alpha","\\beta","\\gamma","\\delta","\\epsilon","\\mu","\\nu","\\xi","\\pi","\\rho",
-				"\\omega","\\Omega","\\theta","\\Delta","\\Psi","\\eta","\\lambda","\\sigma","\\tau",
-				"\\chi","\\phi","\\infty"
-		};
-		return new IconSelectionView(getResourceImage(ALPHABET_ICON),5,5,ctes,strings.getObservableProperty(GREEK_ALPHABET));
-	}
+    private IconSelectionView setGreekAlphabet() {
+        final String[] ctes = { 
+                "\\alpha", "\\beta", "\\gamma", "\\delta", "\\epsilon", "\\mu", "\\nu", "\\xi", "\\pi", "\\rho", 
+                "\\omega", "\\Omega", "\\theta", "\\Delta", "\\Psi", "\\eta", "\\lambda", "\\sigma", "\\tau", 
+                "\\chi", "\\phi", "\\infty"
+        };
+        return new IconSelectionView(getResourceImage(ALPHABET_ICON), 5, 5, ctes, strings.getObservableProperty(GREEK_ALPHABET));
+    }
 
-	public void setOnClick(ActionListener onClick) { 
-		operators    .setActionListener(onClick);
-		greekAlphabet.setActionListener(onClick);
-	}
+    public void setOnClick(ActionListener onClick) { 
+        operators    .setActionListener(onClick);
+        greekAlphabet.setActionListener(onClick);
+    }
 }
