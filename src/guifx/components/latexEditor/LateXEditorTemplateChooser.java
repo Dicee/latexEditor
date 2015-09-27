73896e0b3b78ef4cb527255fa4125e8a1aabe1e0
Dicee <dcourtinot@gmail.com>
2015-09-27 23:46:52 +0100

    Minor cleanup

--- src/guifx/components/latexEditor/LateXEditorTemplateChooser.java
+++ src/guifx/components/latexEditor/LateXEditorTemplateChooser.java
@@ -1,15 +1,17 @@
 package guifx.components.latexEditor;
 
 import static guifx.utils.Settings.bindProperty;
+import static javafx.geometry.Pos.CENTER;
+import static javafx.geometry.Side.RIGHT;
 import static latex.elements.Templates.TEMPLATES;
+import static properties.LanguageProperties.SHOW_AVAILABLE_TEMPLATES;
+import static properties.LanguageProperties.TITLE;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.Side;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
@@ -30,35 +32,33 @@ public class LateXEditorTemplateChooser extends BorderPane {
 
 	private void buildShowMenu() {
 		Button showMenu = new Button();
-		bindProperty(showMenu.textProperty(),"showAvailableTemplates");
+		bindProperty(showMenu.textProperty(), SHOW_AVAILABLE_TEMPLATES);
 		showMenu.setOnAction(ev -> {
-			if (!templatesList.isShowing())
-				templatesList.show(showMenu,Side.RIGHT,0,0);
-			else
-				templatesList.hide();
+			if (!templatesList.isShowing()) templatesList.show(showMenu, RIGHT, 0, 0);
+			else             				templatesList.hide();
 		});
 
 		setTop(showMenu);
 		setPadding(new Insets(15));
 		setPrefHeight(300);
-		setAlignment(showMenu,Pos.CENTER);
-		setMargin(showMenu,new Insets(10,10,30,10));
+		setAlignment(showMenu, CENTER);
+		setMargin(showMenu, new Insets(10, 10, 30, 10));
 	}
 
 	private final void buildPopupMenu(Template template) {
-		Map<String,List<Template>> templatesToShow = 
+		Map<String, List<Template>> templatesToShow = 
 			TEMPLATES.entrySet().stream()
-				.filter(entry-> template.getType().equals("title") ^ !entry.getKey().equals("titlePage"))
-				.collect(Collectors.toMap(entry -> entry.getKey(),entry -> entry.getValue()));
+				.filter(entry -> template.getType().equals(TITLE) ^ !entry.getKey().equals("titlePage"))
+				.collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
 				
 		for (Map.Entry<String,List<Template>> entry : templatesToShow.entrySet())
-			createMenuBuilderFromTemplateTitle(entry.getKey(),template,entry.getValue());
+			createMenuBuilderFromTemplateTitle(entry.getKey(), template, entry.getValue());
 	}
 	
 	private void buildTemplateForm(Template template) {
 		TemplateForm form = new TemplateForm(template);
 		setCenter(form);
-		setAlignment(form,Pos.CENTER);
+		setAlignment(form, CENTER);
 	}
 	
 	private final void createMenuBuilderFromTemplateTitle(String title, Template template, List<Template> availableTemplates) {
@@ -69,7 +69,7 @@ public class LateXEditorTemplateChooser extends BorderPane {
 				template.copyFrom(availableTemplate);
 				TemplateForm form = new TemplateForm(template);
 				setCenter(form);
-				BorderPane.setAlignment(form,Pos.CENTER);
+				BorderPane.setAlignment(form, CENTER);
 			});
 			menu.getItems().add(item);
 		});
